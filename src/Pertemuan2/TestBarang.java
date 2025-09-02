18b4b2f30976444d0087341a62aace7db6ad1414
rsyadipras <rsyadipras@gmail.com>
2025-09-03 00:57:19 +0700

    P2_Percobaan3

---
+++ src/Pertemuan2/TestBarang.java
@@ -0,0 +1,14 @@
+package Pertemuan2;
+
+public class TestBarang {
+    public static void main(String[] args) {
+        Barang brg1 = new Barang();
+        brg1.namaBrg = "Pensil";
+        brg1.jenisBrg = "ATK";
+        brg1.stok = 10;
+        brg1.tampilBarang();
+
+        // menampilkan dan mengisi argumen untuk menambahkan stok barang
+        System.out.println("Stok baru: " + brg1.tambahStok(20));
+    }
+}
