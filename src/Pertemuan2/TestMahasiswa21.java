f93b21fa1e8c6facaa06247cda648879ff76b800
rsyadipras <rsyadipras@gmail.com>
2025-09-02 19:31:02 +0700

    P2_Percobaan2

---
+++ src/Pertemuan2/TestMahasiswa21.java
@@ -0,0 +1,27 @@
+package Pertemuan2;
+
+public class TestMahasiswa21 {
+    public static void main(String[] args) {
+        Mahasiswa21 mhs1 = new Mahasiswa21();
+        mhs1.nim = 101;
+        mhs1.nama = "Lestari";
+        mhs1.alamat = "Jl. Vinolia No 1A";
+        mhs1.kelas = "1A";
+        mhs1.tampilBiodata();
+
+        Mahasiswa21 mhs2 = new Mahasiswa21();
+        mhs2.nim = 102;
+        mhs2.nama = "Adi";
+        mhs2.alamat = "Jl. Sukun No 2B";
+        mhs2.kelas = "1B";
+        mhs2.tampilBiodata();
+
+        Mahasiswa21 mhs3 = new Mahasiswa21();
+        mhs3.nim = 103;
+        mhs3.nama = "Luna";
+        mhs3.alamat = "Jl. Kalpataru No 3C";
+        mhs3.kelas = "1C";
+        mhs3.tampilBiodata();
+
+    }
+}
