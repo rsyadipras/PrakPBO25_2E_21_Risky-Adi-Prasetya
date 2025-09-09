d4fc4f79b97e8db94b1f8f9942d9168ef7a2c46a
rsyadipras <rsyadipras@gmail.com>
2025-09-09 13:36:11 +0700

    P3_Percobaan4

--- src/Pertemuan3/Anggota.java
+++ src/Pertemuan3/Anggota.java
@@ -5,6 +5,12 @@ public class Anggota {
     private String alamat;
     private float simpanan;
 
+    Anggota(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.simpanan = 0;
+    }
+
     public void setNama(String nama) {
         this.nama = nama;
     }
@@ -17,19 +23,19 @@ public class Anggota {
         return nama;
     }
 
-    public String getAlamat(){
+    public String getAlamat() {
         return alamat;
     }
 
-    public float getSimpanan(){
+    public float getSimpanan() {
         return simpanan;
     }
 
-    public void setor(float uang){
+    public void setor(float uang) {
         simpanan += uang;
     }
 
-    public void pinjam(float uang){
+    public void pinjam(float uang) {
         simpanan -= uang;
     }
 }
